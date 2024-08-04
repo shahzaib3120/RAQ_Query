1225b9e7e2b73b4df9fe44c4a408476ae82f7f63
Maha Alassaf <85122442+MahaaAlassaf@users.noreply.github.com>
2024-08-04 10:41:52 +0300

    Your commit message here

--- web/src/components/Search.tsx
+++ web/src/components/Search.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchBooks, setLimit } from "../store/bookSlice";
-import { AppDispatch } from "../store";
-import { UnknownAction } from "@reduxjs/toolkit";
+import { RootState, AppDispatch } from "../store"; 
 
 interface SearchProps {
   title: string;
@@ -11,9 +10,19 @@ interface SearchProps {
 
 const Search: React.FC<SearchProps> = ({ title, setTitle }) => {
   const dispatch = useDispatch<AppDispatch>();
+  const limit = useSelector((state: RootState) => state.books.limit);
 
+  // Handle limit change when the user selects a different option
   const handlePerPageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    dispatch(setLimit(Number(event.target.value)));
+    const newLimit = Number(event.target.value);
+    dispatch(setLimit(newLimit));
+    // Fetch books with the new limit
+    dispatch(fetchBooks({ title, limit: newLimit, offset: 0 }));
+  };
+
+  // Handle search action
+  const handleSearch = () => {
+    dispatch(fetchBooks({ title, limit, offset: 0 }));
   };
 
   return (
@@ -28,10 +37,9 @@ const Search: React.FC<SearchProps> = ({ title, setTitle }) => {
           className="px-5 py-1 bg-blue-900 text-white rounded mr-2 w-full"
         />
         <button
-          onClick={() => {
-            dispatch(fetchBooks({ title, limit: 10, offset: 0 }) as unknown as UnknownAction);
-          }}
-          className="px-4 py-2 bg-blue-900 text-white rounded">
+          onClick={handleSearch}
+          className="px-4 py-2 bg-blue-900 text-white rounded"
+        >
           Search
         </button>
       </div>
@@ -42,7 +50,9 @@ const Search: React.FC<SearchProps> = ({ title, setTitle }) => {
         <select
           id="perPage"
           onChange={handlePerPageChange}
-          className="px-2 py-1 bg-blue-900 text-white rounded">
+          value={limit} // Reflect the current limit value in the select
+          className="px-2 py-1 bg-blue-900 text-white rounded"
+        >
           <option value={10}>10</option>
           <option value={20}>20</option>
           <option value={30}>30</option>
